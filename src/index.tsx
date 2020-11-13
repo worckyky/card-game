2b21b50fe2fce8c64b2b31fb8919cec537bd73d5
Анатолий Митрошин <mymailtolaaa@gmail.com>
2020-11-13 16:12:57 +0300

    change on routing setting to hash route

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from "./BLL/redux-store";
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
